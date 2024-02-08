4bb8431de6bc4eb37c1ce5fe0e26efe4fe2fb52b
Huy Nguyen <11167662+HuyNguyen7994@users.noreply.github.com>
2024-02-08 21:21:26 +0700

    try to get it working

--- infrastructure/init.sh
+++ infrastructure/init.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+sudo -i
 # Update package lists and install dependencies
-sudo yum update -y
-sudo yum install -y git
+yum update -y
+yum install -y git
 
 # Install Node.js 18
-curl -fsSL https://rpm.nodesource.com/setup_18.x | sudo bash -
-sudo yum install -y nodejs
+curl -fsSL https://rpm.nodesource.com/setup_18.x | bash -
+yum install -y nodejs
 
 # Install Pip
-sudo yum install python3-pip -y
+yum install python3-pip -y
+
+# Install Webserver
+yum install httpd -y
+service httpd start
+
+# Clone the Project
+export VITE_BACKEND_URL=http://localhost:8080/
+git clone https://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@github.com/HuyNguyen7994/the_project.git
