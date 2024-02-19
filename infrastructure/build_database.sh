8516caa2e47bd5283ff2d833b92990223ca91cbd
Huy Nguyen <11167662+HuyNguyen7994@users.noreply.github.com>
2024-02-19 13:06:04 +0700

    update

--- infrastructure/build_database.sh
+++ infrastructure/build_database.sh
@@ -3,4 +3,12 @@ sudo dnf install postgresql15.x86_64 postgresql15-server -y
 sudo postgresql-setup --initdb
 sudo systemctl start postgresql
 sudo systemctl enable postgresql
-sudo -i -u postgres psql -f init_database.sql
+sudo -i -u postgres psql -c "create table historical_pm25 (
+    etl_ts timestamp with time zone,
+    station_id bigint,
+    city_name text,
+    station_ts timestamp with time zone,
+    pm25_value smallint
+);
+create index historical_pm25_idx on historical_pm25 (station_id, station_ts);
+"
